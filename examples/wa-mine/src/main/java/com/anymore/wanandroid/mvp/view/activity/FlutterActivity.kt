c7a3804c20299e378815f279d6e21c23d0feb8ad
anymore <any_lymao@qq.com>
2020-04-10 11:44:24 +0800

    FlutterActivity增加动画过渡界面避免黑屏

--- examples/wa-mine/src/main/java/com/anymore/wanandroid/mvp/view/activity/FlutterActivity.kt
+++ examples/wa-mine/src/main/java/com/anymore/wanandroid/mvp/view/activity/FlutterActivity.kt
@@ -2,11 +2,13 @@ package com.anymore.wanandroid.mvp.view.activity
 
 import android.os.Bundle
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
 import com.anymore.andkit.lifecycle.activity.IActivity
+import com.anymore.wanandroid.mvp.view.widget.FlutterLoadingView
 import com.anymore.wanandroid.route.MINE_FLUTTER
 import io.flutter.facade.Flutter
 
@@ -19,18 +21,29 @@ class FlutterActivity : AppCompatActivity(), IActivity {
 
     @Autowired
     @JvmField
-    var initialRoute:String? = ""
+    var initialRoute: String? = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         ARouter.getInstance().inject(this)
         super.onCreate(savedInstanceState)
+        val flutterContainer = findViewById<FrameLayout>(android.R.id.content)
         val flutter = Flutter.createView(this, lifecycle, initialRoute)
-        addContentView(
+        val flutterLoading = FlutterLoadingView(this)
+        flutterContainer.addView(
             flutter,
             ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT
             )
         )
+        flutterContainer.addView(
+            flutterLoading, ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+        )
+        flutter.addFirstFrameListener {
+            flutterLoading.removeSelf()
+        }
     }
 }
